d5a8c1464eb64e24e1d269f596e59bb2c8faa7f2
Bill Sacks <sacks@ucar.edu>
2017-07-26 15:03:05 -0600

    add another diagnostic

--- testing.f90
+++ testing.f90
@@ -7,7 +7,8 @@ program testing
 
   print *, 'ieee_support_nan: ', ieee_support_nan()
   my_nan = transfer(dsnan_pat, my_nan)
-  print *, 'ieee_class: ', ieee_class(my_nan)
-  print *, 'ieee_is_nan: ', ieee_is_nan(my_nan)
+  print *, 'my_nan ieee_class: ', ieee_class(my_nan)
+  print *, 'ieee_class(my_nan) == ieee_signaling_nan: ', ieee_class(my_nan) == ieee_signaling_nan
+  print *, 'my_nan ieee_is_nan: ', ieee_is_nan(my_nan)
 
 end program testing
