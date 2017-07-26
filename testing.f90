138bdd29ec65fc04b4a2dca8bf6af77c0a2a27bf
Bill Sacks <sacks@ucar.edu>
2017-07-26 14:57:53 -0600

    Add test code

---
+++ testing.f90
@@ -0,0 +1,13 @@
+program testing
+  use ieee_arithmetic
+  integer, parameter :: i8 = selected_int_kind(13)
+  integer, parameter :: r8 = selected_real_kind(12)
+  integer(i8), parameter :: dsnan_pat = int(Z'7FF4000000000000',i8)
+  real(r8) :: my_nan
+
+  print *, 'ieee_support_nan: ', ieee_support_nan()
+  my_nan = transfer(dsnan_pat, my_nan)
+  print *, 'ieee_class: ', ieee_class(my_nan)
+  print *, 'ieee_is_nan: ', ieee_is_nan(my_nan)
+
+end program testing
