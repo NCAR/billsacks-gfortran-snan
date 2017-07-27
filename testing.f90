f13fb98f88e9400dd641dc2e5b7202013197c1f7
Bill Sacks <sacks@ucar.edu>
2017-07-27 05:14:44 -0600

    allow selecting the method for checking whether we have a NaN

--- testing.f90
+++ testing.f90
@@ -5,10 +5,12 @@ program testing
   integer(i8), parameter :: dsnan_pat = int(Z'7FF4000000000000',i8)
   integer(i8), parameter :: dqnan_pat = int(Z'7FF8000000000000',i8)
   real(r8) :: my_nan
-  character(len=64) :: mode
   type(ieee_class_type) :: my_class
+  character(len=64) :: mode
+  character(len=64) :: check_method
 
   call get_command_argument(1, mode)
+  call get_command_argument(2, check_method)
 
   print *, 'ieee_support_nan: ', ieee_support_nan()
 
@@ -26,22 +28,30 @@ program testing
      print *, 'With ieee_value of ieee_signaling_nan: '
      my_nan = ieee_value(my_nan, ieee_signaling_nan)
   case default
-     print *, 'call with one argument: transfer_qnan, transfer_snan, ieee_qnan or ieee_snan'
+     print *, 'first argument should be one of: transfer_qnan, transfer_snan, ieee_qnan or ieee_snan'
   end select
 
-  print *, 'about to call ieee_is_nan'
-  print *, 'ieee_is_nan: ', ieee_is_nan(my_nan)
-
-  print *, 'about to call ieee_class'
-  my_class = ieee_class(my_nan)
-  if (my_class == ieee_signaling_nan) then
-     print *, 'signaling nan'
-  else if (my_class == ieee_quiet_nan) then
-     print *, 'quiet nan'
-  else if (my_class == ieee_other_value) then
-     print *, 'other value'
-  else
-     print *, 'UNKNOWN'
-  end if
+  select case (check_method)
+  case('ieee_is_nan')
+     print *, 'about to call ieee_is_nan'
+     print *, 'ieee_is_nan: ', ieee_is_nan(my_nan)
+  case('isnan')
+     print *, 'about to call isnan'
+     print *, 'isnan: ', isnan(my_nan)
+  case('ieee_class')
+     print *, 'about to call ieee_class'
+     my_class = ieee_class(my_nan)
+     if (my_class == ieee_signaling_nan) then
+        print *, 'signaling nan'
+     else if (my_class == ieee_quiet_nan) then
+        print *, 'quiet nan'
+     else if (my_class == ieee_other_value) then
+        print *, 'other value'
+     else
+        print *, 'UNKNOWN'
+     end if
+  case default
+     print *, 'second argument should be one of: ieee_is_nan, isnan, ieee_class'
+  end select
 
 end program testing
