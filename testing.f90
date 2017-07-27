72c2abceef4d4033944e6305dea388add941a1fa
Bill Sacks <sacks@ucar.edu>
2017-07-27 04:58:06 -0600

    Remove internal subroutine, add some print statements
    I realized that some of the deaths were happening in the call to the
    subroutine: gfortran dies with an error if you try to call a routine
    with NaN, as makes sense - so, I'm getting rid of the subroutine call to
    fix this. Also introducing some prints to better see what's going on.

--- testing.f90
+++ testing.f90
@@ -6,6 +6,7 @@ program testing
   integer(i8), parameter :: dqnan_pat = int(Z'7FF8000000000000',i8)
   real(r8) :: my_nan
   character(len=64) :: mode
+  type(ieee_class_type) :: my_class
 
   call get_command_argument(1, mode)
 
@@ -15,41 +16,32 @@ program testing
   case('transfer_qnan')
      print *, 'With transfer of dqnan: '
      my_nan = transfer(dqnan_pat, my_nan)
-     call print_info(my_nan)
   case('transfer_snan')
      print *, 'With transfer of dsnan: '
      my_nan = transfer(dsnan_pat, my_nan)
-     call print_info(my_nan)
   case('ieee_qnan')
      print *, 'With ieee_value of ieee_quiet_nan: '
      my_nan = ieee_value(my_nan, ieee_quiet_nan)
-     call print_info(my_nan)
   case('ieee_snan')
      print *, 'With ieee_value of ieee_signaling_nan: '
      my_nan = ieee_value(my_nan, ieee_signaling_nan)
-     call print_info(my_nan)
   case default
      print *, 'call with one argument: transfer_qnan, transfer_snan, ieee_qnan or ieee_snan'
   end select
 
-contains
-  subroutine print_info(var)
-    real(r8) :: var
-    type(ieee_class_type) :: my_class
+  print *, 'about to call ieee_is_nan'
+  print *, 'ieee_is_nan: ', ieee_is_nan(my_nan)
 
-    print *, 'ieee_is_nan: ', ieee_is_nan(var)
-
-    my_class = ieee_class(var)
-    if (my_class == ieee_signaling_nan) then
-       print *, 'signaling nan'
-    else if (my_class == ieee_quiet_nan) then
-       print *, 'quiet nan'
-    else if (my_class == ieee_other_value) then
-       print *, 'other value'
-    else
-       print *, 'UNKNOWN'
-    end if
-
-  end subroutine print_info
+  print *, 'about to call ieee_class'
+  my_class = ieee_class(my_nan)
+  if (my_class == ieee_signaling_nan) then
+     print *, 'signaling nan'
+  else if (my_class == ieee_quiet_nan) then
+     print *, 'quiet nan'
+  else if (my_class == ieee_other_value) then
+     print *, 'other value'
+  else
+     print *, 'UNKNOWN'
+  end if
 
 end program testing
